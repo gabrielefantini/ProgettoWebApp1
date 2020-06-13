d26ed550de93d25be5e63c2f9b83987a16fbf578
gabrielefantini <gabrielefantini97@gmail.com>
2020-06-13 12:45:36 +0200

    fine parte strutturale Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Home from './components/Home';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
+  //Home input props mokup
   const categorie=[
     {name:"A", isChecked:false},
     {name:"B", isChecked:true}
@@ -14,11 +16,24 @@ function App() {
   const handleChek = (name) => console.log(name);
 
   const cars=[
-    {brand:"fiat", name:"punto", coast:30000000}
+    {brand:"fiat", name:"punto", coast:30000000},
+    {brand:"audi", name:"panda", coast:3000},
+    {brand:"fiat", name:"punto", coast:30000000},
+    {brand:"audi", name:"panda", coast:3000},
+    {brand:"fiat", name:"punto", coast:30000000},
+    {brand:"audi", name:"panda", coast:3000},
+    {brand:"fiat", name:"punto", coast:30000000},
+    {brand:"audi", name:"panda", coast:3000},
+    {brand:"fiat", name:"punto", coast:30000000},
+    {brand:"audi", name:"panda", coast:3000},
   ];
-
-  return (
+  //end of home input props mokup
+  
+  /*return (
     <Home category={categorie} brand={marche} handleCheck={handleChek} cars={cars}></Home>
+  ); */
+  return(
+    <Login></Login>
   );
 }
 
--- client/src/utils/List.js
+++ client/src/utils/List.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
+
 
 export default function List({listElements,...rest}){
     return(
-        <>
+        <Row>
             {listElements.map((element, index) => (
-                <ListElement key={index} {...element}/>
+                <Col md={6} lg={4} xl={2} >
+                    <ListElement key={index} {...element}/>
+                </Col>
             ))}
-        </>
+        </Row>
     );
 }
 
 function ListElement({brand, name, coast, ...rest}){
     return(
-        {brand},{name}, {coast}
+        <Card>
+            <Card.Body>
+            <Card.Subtitle className="mb-2 text-muted">{brand}</Card.Subtitle>
+            <Card.Title>{name}</Card.Title>
+            {coast? (
+                <Card.Text>
+            ‎       €{coast}
+                </Card.Text>
+            ):(<div></div>)}
+            </Card.Body>
+        </Card>
     );
 }
