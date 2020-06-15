24b9ab74770752fb0ab546eae99395196486aac5
gabrielefantini <gabrielefantini97@gmail.com>
2020-06-15 12:42:35 +0200

    commit

--- client/src/components/User/UserRent.js
+++ client/src/components/User/UserRent.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {} from 'react-bootstrap';
 import SecondaryWindow from '../../utils/SecondaryWindow';
+import DatePicker from 'react-datepicker';
 
 export default function UserRent(props){
     return(
         <SecondaryWindow title="Noleggia una macchina">
-
+            <RentForm></RentForm>
         </SecondaryWindow>
     );
 }
 
 function RentForm(props){
-    return();
+    const [startDate, setStartDate] = React.useState(new Date());
+    return(
+        <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+        );
 }
 
 function RentResults(props){
